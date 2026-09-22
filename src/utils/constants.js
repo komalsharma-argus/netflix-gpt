779c5376cfab5594e2e8ecbcf5d875f6c686f6ac
Komal Sharma <komalsharmaofficial2023@gmail.com>
2026-09-22 22:39:40 +0530

    TMDB API data fetch & MainContainer of /browse page with VideoBackground

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDUyNTdhMTljNWE3ODQyYmM4ZmQzZGNjZTNiODIxNyIsIm5iZiI6MTc5MDA3OTc2MS44NzY5OTk5LCJzdWIiOiI2YWIyNzMxMTNlN2IwMzYwZmJkMmM0NzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Rnj08-_4KVYycKYPOIfnwyrtV4uqoGQTvxgNGAXKV4M",
+  },
+};
